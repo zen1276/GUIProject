10f8e52887897263a24d6e4c5a663c9d00cfd1ef
Van hoffman <zvan1276@740s-pl-119.local>
2018-11-20 09:53:20 -0700

    added color changing into the app

--- src/gui/view/FirstFrame.java
+++ src/gui/view/FirstFrame.java
@@ -1,6 +1,27 @@
 package gui.view;
 
-public class FirstFrame
-{
+import javax.swing.JFrame;
+import gui.controller.GUIController;
 
+public class FirstFrame extends JFrame
+{
+	private GUIController appController;
+	private FirstPanel appPanel;
+	public FirstFrame()
+	{
+		super();
+		this.appController = appController;
+		this.appPanel = new FirstPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame
+	{
+		this.setContentPane(appPanel);
+		this.setSize(800, 800);;
+		this.setTitle("Here's Johnny!");
+		this.setResizable(true);
+		this.setVisible(true);
+	}
+	
 }
